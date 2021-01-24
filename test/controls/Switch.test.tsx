0961dfde0efa167679c490bb8df86a36b8dd1b70
Craig Miller <craigmiller160@gmail.com>
2021-01-24 15:42:09 -0500

    Fixed eslint issues

--- test/controls/Switch.test.tsx
+++ test/controls/Switch.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import Switch from '../../src/controls/Switch';
 import { render, waitFor, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import Switch from '../../src/controls/Switch';
 
 interface Form {
     field: boolean;
