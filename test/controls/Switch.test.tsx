c3cad7c4b843b18a5063b8cae8d6095dd33f4050
Craig Miller <craigmiller160@gmail.com>
2021-01-24 15:38:06 -0500

    Setup test

--- test/controls/Switch.test.tsx
+++ test/controls/Switch.test.tsx
@@ -1,4 +1,34 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import Switch from '../../src/controls/Switch';
+
+interface Form {
+    field: boolean;
+}
+
+const onSubmit = jest.fn();
+
+const FormComponent = () => {
+    const { control, handleSubmit } = useForm<Form>({
+        mode: 'onBlur',
+        reValidateMode: 'onChange',
+        defaultValues: {
+            field: false
+        }
+    });
+
+    return (
+        <div>
+            <form onSubmit={ handleSubmit(onSubmit) }>
+                <Switch
+                    name="field"
+                    control={ control }
+                    label="The Field"
+                />
+            </form>
+        </div>
+    );
+};
 
 describe('Switch', () => {
     it('works', () => {
