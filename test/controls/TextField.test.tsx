58cf76728001c293960d4f9cde237b3ebdb1319d
Craig Miller <craigmiller160@gmail.com>
2021-01-24 14:08:42 -0500

    Stubbed out tests

--- test/controls/TextField.test.tsx
+++ test/controls/TextField.test.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 describe('TextField', () => {
-    it('test', () => {
+    it('accepts input for type text', () => {
+        throw new Error();
+    });
+
+    it('accepts input for type number', () => {
+        throw new Error();
+    })
+
+    it('displays error message for validation rules', () => {
+        throw new Error();
+    });
+
+    it('transforms input', () => {
+        throw new Error();
+    });
+
+    it('is disabled', () => {
         throw new Error();
     });
 });
