559072880d985436a46f464a6242c9fda2e73d35
Craig Miller <craigmiller160@gmail.com>
2022-06-30 09:46:03 -0400

    Trying to fix the testid

--- src/controls/TextField.tsx
+++ src/controls/TextField.tsx
@@ -54,7 +54,9 @@ const TextField = <F extends FieldValues>(props: Props<F>) => {
                     id={ id }
                     type={ type }
                     className={ className }
-                    data-testid={ props.testId }
+                    inputProps={{
+                        'data-testid': props.testId
+                    }}
                     label={ label }
                     placeholder={ placeholder }
                     onChange={ (event) => {
