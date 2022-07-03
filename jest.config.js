8172b2c84b59224ac7420ebc634967e3e1d319dd
Craig Miller <craigmiller160@gmail.com>
2022-07-03 10:44:02 -0400

    More progress

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 const jestConfig = require('@craigmiller160/jest-config');
 const jestConfigTs = require('@craigmiller160/jest-config-ts');
 const configMerge = require('@craigmiller160/config-merge');
+const path = require('path');
 
-module.exports = configMerge(jestConfig, jestConfigTs);
+module.exports = configMerge(jestConfig, jestConfigTs, {
+	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setupTests.ts')]
+});
