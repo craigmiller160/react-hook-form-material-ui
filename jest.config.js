4209a08a5ba767c409bc0d615bd803ff4b878055
Craig Miller <craigmiller160@gmail.com>
2022-07-03 10:38:52 -0400

    Setting up jest again

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
+const jestConfig = require('@craigmiller160/jest-config');
+const jestConfigTs = require('@craigmiller160/jest-config-ts');
+const configMerge = require('@craigmiller160/config-merge');
 
-const jestConfig = require('@craigmiller160/react-web-config/jest');
-
-module.exports = {
-    ...jestConfig
-};
+module.exports = configMerge(jestConfig, jestConfigTs);
