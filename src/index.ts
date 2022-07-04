09744093d657db07889384ed816c1715d2f9972d
Craig Miller <craigmiller160@gmail.com>
2022-07-04 14:06:13 -0400

    Exported DatePicker

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as Switch } from './controls/Switch';
 export { default as Autocomplete } from './controls/Autocomplete';
 export { FileChooser } from './controls/FileChooser';
 export { Select } from './controls/Select';
+export { DatePicker } from './controls/DatePicker';
 export * from './types/form';
