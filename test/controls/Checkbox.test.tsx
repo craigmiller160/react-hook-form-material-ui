f9e265c9e4db2d096e74e2562bd14c962397159e
Craig Miller <craigmiller160@gmail.com>
2022-07-05 09:18:28 -0400

    Implemented a test

--- test/controls/Checkbox.test.tsx
+++ test/controls/Checkbox.test.tsx
@@ -1,7 +1,53 @@
-export {};
+import { useForm } from 'react-hook-form';
+import { Checkbox } from '../../src';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+interface Form {
+	readonly field: boolean;
+}
+
+interface FormComponentProps {
+	readonly onSubmit: (values: Form) => void;
+}
+
+const FormComponent = (props: FormComponentProps) => {
+	const { control, handleSubmit } = useForm<Form>({
+		defaultValues: {
+			field: false
+		}
+	});
+	return (
+		<form onSubmit={handleSubmit(props.onSubmit)}>
+			<Checkbox control={control} name="field" label="My Checkbox" />
+			<button type="submit">Submit</button>
+		</form>
+	);
+};
 
 describe('Checkbox', () => {
-	it('can check box', () => {
+	let receivedValues: Form | undefined;
+	beforeEach(() => {
+		receivedValues = undefined;
+	});
+
+	it('can check box', async () => {
+		render(
+			<FormComponent
+				onSubmit={(values) => {
+					receivedValues = values;
+				}}
+			/>
+		);
+		await userEvent.click(screen.getByLabelText('My Checkbox'));
+		await userEvent.click(screen.getByText('Submit'));
+		expect(receivedValues).toEqual({
+			field: true
+		});
+	});
+
+	it('does dynamic submit', async () => {
 		throw new Error();
 	});
 });
