63cba9d7d3e717cb7ede60fcc40f7189179e9ace
Craig Miller <craigmiller160@gmail.com>
2022-07-06 11:50:16 -0400

    More id tweaks on checkbox

--- src/controls/Checkbox.tsx
+++ src/controls/Checkbox.tsx
@@ -2,29 +2,35 @@ import { Controller, FieldValues } from 'react-hook-form';
 import { FormControlLabel } from '@mui/material';
 import MuiCheckbox from '@mui/material/Checkbox';
 import { DefaultProps } from '../types/form';
+import { useId } from 'react';
 
-export const Checkbox = <F extends FieldValues>(props: DefaultProps<F>) => (
-	<Controller
-		name={props.name}
-		control={props.control}
-		rules={props.rules}
-		render={({ field }) => (
-			<FormControlLabel
-				id={props.id}
-				label={props.label}
-				control={
-					<MuiCheckbox
-						{...field}
-						data-testid={props.testId}
-						className={props.className}
-						disabled={props.disabled}
-						onChange={(event) => {
-							field.onChange(event);
-							props.onValueHasChanged?.();
-						}}
-					/>
-				}
-			/>
-		)}
-	/>
-);
+export const Checkbox = <F extends FieldValues>(props: DefaultProps<F>) => {
+	const generatedId = useId();
+	const idToUse = props.id ?? generatedId;
+	return (
+		<Controller
+			name={props.name}
+			control={props.control}
+			rules={props.rules}
+			render={({ field }) => (
+				<FormControlLabel
+					htmlFor={idToUse}
+					label={props.label}
+					control={
+						<MuiCheckbox
+							{...field}
+							id={idToUse}
+							data-testid={props.testId}
+							className={props.className}
+							disabled={props.disabled}
+							onChange={(event) => {
+								field.onChange(event);
+								props.onValueHasChanged?.();
+							}}
+						/>
+					}
+				/>
+			)}
+		/>
+	);
+};
